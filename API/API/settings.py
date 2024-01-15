0985017029b013e71a95a96851dfb40781d67cfe
Matias Colil Colil <matias.colil@usach.cl>
2024-01-15 19:37:30 -0300

    static files

--- API/API/settings.py
+++ API/API/settings.py
@@ -155,6 +155,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# Configuración para archivos estáticos en producción
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
